1d5fd3795841155c5400871f80c69c90f94ce6ba
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-28 14:18:36 -0300

    initizated creation menu navigation

---
+++ src/components/menuNavigation.tsx
@@ -0,0 +1,14 @@
+
+
+export default function MenuNavigation() {
+	return (
+		<header>
+			<nav>
+				<ul>
+                    <li><a href="/">Search File</a></li>
+                    <li><a href="/about">Upload File</a></li>
+                </ul>
+			</nav>
+		</header>
+	)
+}
