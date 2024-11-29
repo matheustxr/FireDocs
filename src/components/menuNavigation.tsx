6bbf8322cb3ac8b84ef74250e630e15de749d3d4
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-29 20:12:41 -0300

    add menu navigation

--- src/components/menuNavigation.tsx
+++ src/components/menuNavigation.tsx
@@ -1,12 +1,24 @@
+import { NavLink } from "react-router-dom"
 
 
 export default function MenuNavigation() {
 	return (
-		<header>
+		<header className="container mx-auto px-5 py-10">
 			<nav>
-				<ul>
-                    <li><a href="/">Search File</a></li>
-                    <li><a href="/about">Upload File</a></li>
+				<ul className=" flex justify-center gap-5 md:gap-10">
+					<NavLink
+						to={"/"}
+						className='text-xl md:text-2xl font-semibold border px-4 py-2.5 rounded border-black'
+					>
+						Search File
+					</NavLink>
+
+					<NavLink
+						to={"/admin-certificate"}
+						className='text-xl md:text-2xl font-semibold border px-4 py-2.5 rounded border-black'
+					>
+						Upload File
+					</NavLink>
                 </ul>
 			</nav>
 		</header>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PublicPage from '../pages/PublicPage';
 import AdminPage from '../pages/AdminPage';
+import MenuNavigation from '../components/menuNavigation';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
+      <MenuNavigation />
       <Routes>
         <Route path="/" element={<PublicPage />} />
-        <Route path="/admin-certificados" element={<AdminPage />} />
+        <Route path="/admin-certificate" element={<AdminPage />} />
       </Routes>
     </Router>
   );
